f6fe1844ca51f6934775395589b19e3daf3e00cd
Sahil Sidam <sahilsidam03@gmail.com>
2024-07-19 19:16:31 +0530

    Implement create meeting and schedule meeting functionalities

--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -1,5 +1,61 @@
-const MeetingSetup = () => {
-  return <div>MeetingSetup</div>;
+"use client";
+import {
+  DeviceSettings,
+  useCall,
+  VideoPreview,
+} from "@stream-io/video-react-sdk";
+import { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+
+const MeetingSetup = ({
+  setIssetupComplete,
+}: {
+  setIssetupComplete: (value: boolean) => void;
+}) => {
+  const [isMicCamToggledOn, setisMicCamToggledOn] = useState(false);
+  const call = useCall();
+
+  if (!call) {
+    throw new Error("useCall must be used in StreamCall component");
+  }
+
+  useEffect(() => {
+    if (isMicCamToggledOn) {
+      call?.camera.disable();
+      call?.microphone.disable();
+    } else {
+      call?.camera.enable();
+      call?.microphone.enable();
+    }
+  }, [isMicCamToggledOn, call?.camera, call?.microphone]);
+  return (
+    <div className="flex h-screen w-full flex-col justify-center items-center gap-3 text-white">
+      <h1 className="text-2xl font-bold">Setup</h1>
+      <VideoPreview />
+      <div className="flex h-16 items-center justify-center gap-3">
+        <label className="flex items-center justify-center gap-2 font-medium">
+          <input
+            type="checkbox"
+            checked={isMicCamToggledOn}
+            onChange={(e) => {
+              setisMicCamToggledOn(e.target.checked);
+            }}
+          />
+          Join with mic and camera off
+        </label>
+        <DeviceSettings />
+      </div>
+      <Button
+        className="rounded-md bg-green-500 px-4 py-2.5"
+        onClick={() => {
+          call.join();
+          setIssetupComplete(true);
+        }}
+      >
+        Join meeting
+      </Button>
+    </div>
+  );
 };
 
 export default MeetingSetup;
