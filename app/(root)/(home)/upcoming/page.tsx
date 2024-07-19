910256bf73e6b7f7bcd74ff603cebdb5f2388616
Sahil Sidam <sahilsidam03@gmail.com>
2024-07-19 21:25:37 +0530

    Implement upcoming, previous and recordings sections

--- app/(root)/(home)/upcoming/page.tsx
+++ app/(root)/(home)/upcoming/page.tsx
@@ -1,7 +1,11 @@
+import CallList from "@/components/CallList";
+
 const Upcoming = () => {
   return (
     <section className="flex size-full flex-col text-white gap-10">
-      <h1 className="text-3xl font-bold">Upcoming</h1>
+      <h1 className="text-3xl font-bold">
+        <CallList type="upcoming" />
+      </h1>
     </section>
   );
 };
